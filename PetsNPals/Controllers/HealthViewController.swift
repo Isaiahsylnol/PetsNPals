0df070d199c56f0efcecae6ca1c23d9d41b28a9a
Isaiah <pictureisaiah@gmail.com>
2021-05-14 21:54:24 -0400

    Added images to post detailed view.

--- PetsNPals/Controllers/HealthViewController.swift
+++ PetsNPals/Controllers/HealthViewController.swift
@@ -8,60 +8,51 @@
 import UIKit
 
 class HealthViewController: UIViewController{
-    private var myArray = [Post]()
-    @IBOutlet weak var healthTableView: UITableView!
-    var posts: [HealthPost] = []
     
+    private var postArray = [Post]()
+    @IBOutlet weak var healthTableView: UITableView!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
  
         let anonymousFunction = { (fetchedUserList: [Post]) in
             DispatchQueue.main.async {
-                self.myArray = fetchedUserList
+                self.postArray = fetchedUserList
                 self.healthTableView.reloadData()
             }
         }
-        
         PostContentAPI.shared.fetchUserList(onCompletion: anonymousFunction)
     }
 }
 
 extension HealthViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myArray.count
+        return postArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          
-        let user = myArray[indexPath.row]
+        let post = postArray[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "HealthPostCell") as! HealthPostCell
-        cell.titleLabel.text = user.title
-        cell.dateLabel.text = user.date
-        cell.paragraphTextArea.text = user.paragraph
         
-        cell.articleImage.image = UIImage(named: user.img!)
+        cell.titleLabel.text = post.title
+        cell.dateLabel.text = post.date
+        cell.paragraphTextArea.text = post.paragraph
+        cell.articleImage.image = UIImage(named: post.img!)
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
  
-        let index = indexPath.row
-        if index == 0 {
-
+        // MARK: - Assign the information for each post at their given index to the "PostDetailsView"
                 if let postDetail = storyboard?.instantiateViewController(withIdentifier: "PostDetailViewController") as? PostDetailViewController{
-                    postDetail.postTitle = myArray[index].title!
+                    postDetail.postTitle = postArray[indexPath.row].title!
+                    postDetail.postImage = UIImage(named: postArray[indexPath.row].img!)!
+                    postDetail.paragraph = postArray[indexPath.row].paragraph!
+                    
                     self.navigationController?.pushViewController(postDetail, animated: true)
-                }
-        }
-        else if index == 2 {
-            let videoView = self.storyboard?.instantiateViewController(withIdentifier: "VideoViewController") as! VideoViewController
-            
-            self.navigationController?.pushViewController(videoView, animated: true)
-        }
-        else if index == 3 {
-            print("Shop")
         }
     }
-
 }
 
--- PetsNPals/Controllers/PostDetailViewController.swift
+++ PetsNPals/Controllers/PostDetailViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 class PostDetailViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var paragraphTextArea: UITextView!
     
     var postTitle = ""
+    var paragraph = ""
+    var postImage = UIImage()
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = postTitle
+        paragraphTextArea.text = paragraph
+        imageView.image = postImage
         
     }
 }
